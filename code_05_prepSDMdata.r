f7879972fcc2a71776a4f3b3ce613d1bcbe2f2cc
Pratik Gupte <pratikgupte16@gmail.com>
2019-03-13 13:15:13 +0100

    elaborate getSDMdata code

--- code_05_prepSDMdata.r
+++ code_05_prepSDMdata.r
@@ -23,3 +23,26 @@ fieldsToKeep = c("locality", "locality_id", "locality_type",
 dataCovar = distinct(dataCovar, locality_id, .keep_all = T) %>% 
   select(fieldsToKeep)
 
+#### join with presence and absence data ####
+
+#'join with dataframe of presences and absences
+#'if a combined dataframe is the objective, do this
+dataPresAbsCovars = dataSpread[["Ficedula nigrorufa"]][["pres_abs"]] %>% 
+  left_join(dataCovar, by = "locality_id")
+
+#'if a standalone dataframe without the presence data is required, do this
+dataCovar = dataCovar %>% 
+  filter(locality_id %in% dataSpread[["Ficedula nigrorufa"]][["pres_abs"]]$locality_id)
+
+#### calling dataframes for functions ####
+#'in general, list elements with names can be called by this notation
+#'list[[first_level_name]][[second_level_name]]
+#'
+#'in this case, see the calling of black-and-orange flycatcher presence data
+#'from the 2 level list dataSpread above
+#'1. level one is the species
+#'2. level two are the variables
+#'
+#'for a full list of all species (14 elements long), each of the
+#'14 elements will have N nested sub-elements, where N is the
+#'number of variables, such as presence_absence, number-of-observers etc.
