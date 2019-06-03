78234411697d8b34395e0eb8bb02521c83bc93b6
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-03 14:28:25 +0200

    count observers > 5 checklists

--- code_07_observerExpertise.r
+++ code_07_observerExpertise.r
@@ -1,3 +1,11 @@
 #### code to explore observer expertise ####
 
-# WIP
+# load libs and data
+library(data.table)
+
+nSpecObs <- fread("data/eBirdChecklistSpecies.csv")
+
+# check observers with more than 10 records
+nSpecObs[,.N,observer][N >= 10, .N]
+
+# how does one choose covariates for observer expertise?
