6c185d48ec746f7718654bdbb3e52b91fc8a3652
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-09 15:17:14 +0200

    specify initial species richness model for data subset

--- code_07_observerExpertise.r
+++ code_07_observerExpertise.r
@@ -3,9 +3,19 @@
 # load libs and data
 library(data.table)
 
-nSpecObs <- fread("data/eBirdChecklistSpecies.csv")
+ebdNspChk <- fread("data/eBirdChecklistSpecies.csv", nrows = 1e3)
 
-# check observers with more than 10 records
-nSpecObs[,.N,observer][N >= 10, .N]
+# remove NAs
+ebdNspChk <- na.omit(ebdNspChk)
 
-# how does one choose covariates for observer expertise?
+# choosing covariates from Kelling et al. 2015
+# covariates reflect different classes of peredictors
+# 1. variability in the species richness (time of year and habitat)
+# 2. variability in species habits (time of day and effort)
+# 3. observer effect (obsever id)
+
+# fit a poisson GLM per Johnston et al. 2018
+library(lme4)
+
+modNspecies <- glmer(N ~ sqrt(samplingEffort) + julianDate + I(julianDate^2) + julianDate + I(julianDate^2) + (1|observer/checklist_id), data = ebdNspChk, family = poisson)
+
