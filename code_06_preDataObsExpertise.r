2b10272ce661a5df476c57b89139352108d2b3e9
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-12 21:03:28 +0200

    subset by wg shapefile

--- code_06_preDataObsExpertise.r
+++ code_06_preDataObsExpertise.r
@@ -1,5 +1,8 @@
 #### code to prepare data for expertise score modelling ####
 
+# to note: habitat type raster is only covers western ghats
+# code has been modified to model expertise only from within the WG shapefile BOUNDS
+
 # to be run as a script
 rm(list = ls()); gc()
 
@@ -7,8 +10,16 @@ rm(list = ls()); gc()
 library(data.table)
 library(stringr)
 
+# read in shapefile of wg to subset by bounding box
+library(sf)
+wg <- st_read("hillsShapefile/Nil_Ana_Pal.shp")
+box <- st_bbox(wg)
+
 # read in data with fread
 ebd = fread("ebd_Filtered_May2018.txt")
+
+#### write subset to file ####
+ebd <- ebd[between(LONGITUDE, box["xmin"], box["xmax"]) & between(LATITUDE, box["ymin"], box["ymax"]),]
 
 # make new column names
 newNames <- str_replace_all(colnames(ebd), " ", "_") %>%
@@ -16,7 +27,7 @@ newNames <- str_replace_all(colnames(ebd), " ", "_") %>%
 setnames(ebd, newNames)
 
 # keep useful columns
-columnsOfInterest = c("checklist_id","scientific_name","observation_count","locality","locality_id","locality_type","latitude","longitude","observation_date","time_observations_started","observer_id","sampling_event_identifier","protocol_type","duration_minutes","effort_distance_km","effort_area_ha","number_observers","species_observed","reviewed","state_code", "group_identifier")
+columnsOfInterest <- c("checklist_id","scientific_name","observation_count","locality","locality_id","locality_type","latitude","longitude","observation_date","time_observations_started","observer_id","sampling_event_identifier","protocol_type","duration_minutes","effort_distance_km","effort_area_ha","number_observers","species_observed","reviewed","state_code", "group_identifier")
 
 ebd <- dplyr::select(ebd, dplyr::one_of(columnsOfInterest))
 
@@ -39,7 +50,7 @@ time_to_decimal <- function(x) {
 
 # get species per checklist and SEI (each SEI is a different location)
 ebdNspChk <- ebd[, .(samplingEffort = max(duration_minutes),
-                     # samplingDistance = max(effort_distance_km),
+                     samplingDistance = max(effort_distance_km),
                      decimalTime = first(time_to_decimal(time_observations_started)),
                      julianDate = first(yday(as.POSIXct(observation_date))),
                      latitude = first(latitude),
@@ -56,10 +67,12 @@ fwrite(ebdNspChk, file = "data/eBirdChecklistSpecies.csv")
 fwrite(ebdNchk, file = "data/eBirdNchecklistObserver.csv")
 
 #### hbaitat type as landcover ####
-# # suspended for now since landcover raster is not the same size as expertise score data
-# # read in raster
-# landcover <- raster::raster("Classified Image_3rdJune2019_WG.tif")
-# 
-# # get for unique points
-# landcoverVec <- raster::extract(x = landcover, y = as.matrix(ebd[,c("longitude","latitude")]))
+# read in raster
+landcover <- raster::raster("Classified Image_3rdJune2019_WG.tif")
 
+# get for unique points
+landcoverVec <- raster::extract(x = landcover, y = as.matrix(ebdNspChk[,c("longitude","latitude")]))
+
+# assign to df and overwrite
+ebdNspChk[,landcover:= landcoverVec]
+fwrite(ebdNspChk, file = "data/eBirdChecklistSpecies.csv")
