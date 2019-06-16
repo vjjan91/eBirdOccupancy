6ebcadbe9605dce969c6ec95f56e5d2cdb43ab7b
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-16 15:00:16 +0200

    explore obs expertise

--- code_07_observerExpertise.r
+++ code_07_observerExpertise.r
@@ -5,7 +5,7 @@ library(data.table)
 
 ebdNspChk <- fread("data/eBirdChecklistSpecies.csv")
 
-# remove NAs - leaves around 95k points
+# remove NAs - leaves around 210k points
 ebdNspChk <- na.omit(ebdNspChk)
 
 # makde factor
@@ -20,14 +20,20 @@ ebdNspChk <- as_tibble(ebdNspChk)
 # 2. variability in species habits (time of day and effort)
 # 3. observer effect (obsever id)
 
+# plot data to explore
+library(ggplot2)
+
+ggplot()+
+  geom_histogram(data = ebdNspChk, aes(x = decimalTime, y = ..density..))
+
 # fit a poisson GLM per Johnston et al. 2018
 library(lme4)
 
 modNspecies <- glmer(N ~ log(samplingEffort) + 
-                       log(julianDate) + I(log(julianDate)^2) + 
-                       # decimalTime + I(decimalTime^2) + 
-                       # landcover + 
-                       (1|observer), 
+                       log(julianDate) + log(julianDate^2)+
+                       decimalTime +
+                       (1|observer) + (1|landcover),
                      data = ebdNspChk %>% dplyr::sample_n(1e3), family = poisson)
 
 library(sjPlot)
+plot_model(modNspecies)
