b21a48abe60c823b9b525a1d17e75608ed5a062f
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-30 20:21:53 +0200

    add summary per checklist

--- code_07_observerExpertise.r
+++ code_07_observerExpertise.r
@@ -11,8 +11,14 @@ ebdNspChk <- na.omit(ebdNspChk)
 # makde factor
 ebdNspChk[,landcover:=as.factor(landcover)]
 
-library(tibble)
-ebdNspChk <- as_tibble(ebdNspChk)
+# summarise
+ebdNspSum <- ebdNspChk[,.(totalEff = sum(samplingEffort),
+                    totalDist = sum(samplingDistance),
+                    startTime = min(decimalTime),
+                    meanDate = mean(julianDate),
+                    nSp = sum(N),
+                    nLand = length(unique(landcover)),
+                    obs = first(observer)), by=checklist_id]
 
 # choosing covariates from Kelling et al. 2015
 # covariates reflect different classes of peredictors
@@ -22,18 +28,47 @@ ebdNspChk <- as_tibble(ebdNspChk)
 
 # plot data to explore
 library(ggplot2)
+library(dplyr); library(tidyr)
 
+ebdNspChk %>% 
+  filter(samplingEffort <= 60 * 10,
+         samplingDistance <= 25) %>%  #assumed km
+  select_if(is.numeric) %>% 
+  gather(variable, value) %>% 
 ggplot()+
-  geom_histogram(data = ebdNspChk, aes(x = decimalTime, y = ..density..))
+  geom_histogram(aes(x = value))+
+  facet_wrap(~variable, scales = "free_x")
 
-# fit a poisson GLM per Johnston et al. 2018
-library(lme4)
+ggsave(filename = "figHistCovars.png", device = png(), height = 6, width = 6); dev.off()
 
-modNspecies <- glmer(N ~ log(samplingEffort) + 
-                       log(julianDate) + log(julianDate^2)+
-                       decimalTime +
-                       (1|observer) + (1|landcover),
-                     data = ebdNspChk %>% dplyr::sample_n(1e3), family = poisson)
 
-library(sjPlot)
-plot_model(modNspecies)
+#### modelling species in checklist ####
+# try a poisson GLM per Johnston et al. 2018
+# library(lme4)
+# 
+# modNspecies <- glmer(N ~ log(samplingEffort) + 
+#                        log(julianDate) + log(julianDate^2)+
+#                        decimalTime +
+#                        (1|observer) + (1|landcover),
+#                      data = ebdNspChk %>% dplyr::sample_n(1e3), family = poisson)
+# 
+# library(sjPlot)
+# plot_model(modNspecies)
+
+# summarise the data
+# sum the samplingEffort, samplingDistance, mean of decimalTime, julianDate, lat, long, sum of N, sum of unique landcovers
+
+library(tibble)
+ebdNspSum <- as_tibble(setDF(ebdNspSum))
+ebdNspSum$obs <- as.factor(ebdNspSum$obs)
+
+# construct a scam
+library(scam)
+library(mgcv)
+library(gamm4)
+
+modNspecies <- gamm4(nSp ~ s(totalEff) + s(startTime), data = ebdNspSum, random = ~(1|obs))
+
+plot.scam(modNspecies, shift = coef(modNspecies)[1], shade = TRUE)
+
+summary(modNspecies)
