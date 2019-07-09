e7f83c37b9c7d8ab30c68c805744e0cc028fc0b9
Pratik Gupte <pratikgupte16@gmail.com>
2019-07-09 15:19:19 +0200

    set up model and predict method

--- code_07_observerExpertise.r
+++ code_07_observerExpertise.r
@@ -48,9 +48,51 @@ modNspecies <- gamm4(nSp ~ s(log(totalEff), k = 5) +
                        s(startTime, bs = "cc") +
                        s(meanDate, bs = "cc") + land, 
                      random = ~(1|obs), 
-                     data = ebdNspSum)
+                     data = ebdNspSum, family = "poisson")
 
 summary(modNspecies$mer)
 
+# save model object
 save(modNspecies, file = "tempExpertiseData.rdata")
 
+#### load model object and fit a curve ####
+load("data/tempExpertiseData.rdata")
+
+summary(modNspecies$gam)
+
+# use predict method
+setDT(ebdNspSum)
+ebdNspSum[,predval:=predict(modNspecies$gam)]
+# round the effort to 10 min intervals
+ebdNspSum[,roundHour:=plyr::round_any(totalEff/60, 0.5, f = floor)]
+
+# summarise the empval, predval grouped by observer and round10min
+pltData <- ebdNspSum[,.(prednspMean = mean(predval, na.rm = T),
+             prednspSD = sd(predval, na.rm = T)),
+          by=list(obs, roundHour)]
+
+{# plot and examine in base R plots
+setDF(pltData)
+
+# filter for 10 data points or more
+pltData <- pltData %>% filter(obs %in% obscount$obs)  
+
+# get limits
+xlims = c(0, 5); ylims = c(0, 100)
+# set up plot
+plot(0, xlim = xlims, ylim = ylims, type = "n", 
+     xlab = "total effort (mins)", ylab = "N species")
+# nest data
+pltData <- tidyr::nest(pltData, -obs)
+# plot in a loop
+for(i in 1:nrow(pltData)){
+  df = pltData$data[[i]]
+  lines(df$roundHour, df$prednspMean, col=alpha(rgb(0,0,0), 0.1))
+}
+
+}
+
+#### get observer scores as n species at 1 hour ####
+ebdNspPred <- ebdNspSum %>% 
+  mutate(totalEff = 60) %>% 
+  distinct(obs, checklist_id, totalEff, startTime, meanDate, land, nLand)
