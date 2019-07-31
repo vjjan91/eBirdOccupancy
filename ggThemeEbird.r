ac9c5d200a98d0f88f74ff4b7ff355a7c556194c
pratik <pratikgupte16@gmail.com>
2019-07-31 18:12:00 +0200

    mod theme for axis etxt size

--- ggThemeEbird.r
+++ ggThemeEbird.r
@@ -11,10 +11,10 @@ themeEbird <- function(base_size=10) {
             axis.title = element_text(face = "plain",size = rel(1)),
             axis.title.y = element_text(angle=90,vjust = 4),
             axis.title.x = element_text(vjust = -2),
-            axis.text.x = element_text(vjust = -1, size = 4),
-            axis.text.y = element_text(angle=90, vjust = 2, hjust = 0.5, size = 4),
+            axis.text.x = element_text(vjust = 1),
+            axis.text.y = element_text(angle=90, vjust = 2, hjust = 0.5),
             axis.ticks = element_line(size = 0.3),
-            axis.ticks.length = unit(-0.1, "cm"),
+            axis.ticks.length = unit(0.2, "cm"),
             panel.grid.major = element_blank(),
             legend.position = "none",
             panel.grid.minor = element_blank(),
@@ -24,7 +24,7 @@ themeEbird <- function(base_size=10) {
             strip.text = element_text(face="bold", hjust = 0),
             panel.spacing = unit(1, "lines")
       ))
-  
+
 }
 
 themeEbirdMap <- function(base_size=10) {
@@ -50,5 +50,5 @@ themeEbirdMap <- function(base_size=10) {
             strip.text = element_text(face="bold", hjust = 0),
             panel.spacing = unit(1, "lines")
       ))
-  
+
 }
