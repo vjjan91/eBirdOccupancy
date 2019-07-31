fbfb3aedb89802c03a24e4a6276c725dc1cb34d0
pratik <pratikgupte16@gmail.com>
2019-07-31 18:11:48 +0200

    redo entire file

--- code_08_observerScoreCutoff.r
+++ code_08_observerScoreCutoff.r
@@ -6,67 +6,85 @@ rm(list = ls()); gc()
 library(data.table)
 library(tidyverse)
 
-# read in shapefile of wg to subset by bounding box
-library(sf)
-wg <- st_read("hillsShapefile/WG.shp"); box <- st_bbox(wg)
+#### data loading ####
+# get ranef score data
+ranefscore <- fread("data/dataObsRptrScore.csv")
 
-# read in data and subset
-ebd = fread("ebd_Filtered_May2018.txt")[between(LONGITUDE, box["xmin"], box["xmax"]) & between(LATITUDE, box["ymin"], box["ymax"]),][year(`OBSERVATION DATE`) >= 2013,]
+# read in nilgiris data
+ebd <- fread("data/dataForUse.csv")
+# keep cols of interest
+ebd <- setDF(ebd) %>% as_tibble() %>%
+  mutate(year = year(observation_date)) %>% 
+  select(observer = observer_id, year, 
+         sei = sampling_event_identifier, scientific_name)
 
-# make new column names
-newNames <- str_replace_all(colnames(ebd), " ", "_") %>%
-  str_to_lower()
-setnames(ebd, newNames)
+# get soi names
+soi <- c("Anthus nilghiriensis",
+         "Montecincla cachinnans",
+         "Montecincla fairbanki",
+         "Sholicola albiventris",
+         "Sholicola major",
+         "Culicicapa ceylonensis",
+         "Pomatorhinus horsfieldii",
+         "Ficedula nigrorufa",
+         "Pycnonotus jocosus",
+         "Iole indica",
+         "Hemipus picatus",
+         "Saxicola caprata",
+         "Eumyias albicaudatus",
+         "Rhopocichla atriceps")
 
-# keep useful columns
-columnsOfInterest <- c("checklist_id","scientific_name","observation_count","locality","locality_id","locality_type","latitude","longitude","observation_date","time_observations_started","observer_id","sampling_event_identifier","protocol_type","duration_minutes","effort_distance_km","effort_area_ha","number_observers","species_observed","reviewed","state_code", "group_identifier")
+#### data handling ####
+# who sees the most species
+ebdSummary <- ebd %>% 
+  inner_join(ranefscore) %>% 
+  mutate(roundscore = plyr::round_any(rptrScore, 0.05)) %>% 
+  group_by(roundscore, year) %>% 
+  summarise(nchk = length(unique(sei)))# %>% 
+  group_by(year) %>% 
+  mutate(prop = nchk/sum(nchk))
 
-ebd <- dplyr::select(ebd, dplyr::one_of(columnsOfInterest))
+# make plot
+#plotObsScoreProp <- 
+  ggplot(ebdSummary)+
+  geom_tile(aes(x = year, y = roundscore, fill = prop))+
+  scale_fill_viridis_c(option = "D", direction = -1, 
+                       name = "proportion seen")+
+  scale_x_continuous(breaks = c(2013:2018))+
+  coord_cartesian(expand = F)+
+  themeEbird()+
+  # theme(legend.position = c(0.8, 0.1),
+  #       legend.direction = "horizontal")+
+  labs(y = "observer score")
 
-gc()
 
-# get the checklist id as SEI or group id
-ebd[,checklist_id := ifelse(group_identifier == "", 
-                            sampling_event_identifier, group_identifier),]
+# for each observer and year combination
+# ask how many and which of the focal species were seen
+# just bloody handle the data
+ebdSoiSummary <- ebd %>% 
+  inner_join(ranefscore) %>% 
+  mutate(roundscore = plyr::round_any(rptrScore, 0.1)) %>% 
+  filter(scientific_name %in% soi) %>% 
+  count(scientific_name, year, roundscore) %>% 
+  mutate(prop = n/sum(n)) %>% 
+  group_by(scientific_name, year) %>% 
+  mutate(prop = n/sum(n))
 
-#### count obs within nilgiris ####
-# load nilgiris
-nl <- st_read("hillsShapefile/Nil_Ana_Pal.shp"); box <- st_bbox(nl)
+#### make plot####
+source("ggThemeEbird.r")
+plotSoiScore <- 
+  ggplot(ebdSoiSummary)+
+  geom_tile(aes(x = year, y = roundscore, fill = prop))+
+  facet_wrap(~scientific_name)+
+  scale_fill_viridis_c(option = "C", direction = -1, 
+                       name = "proportion seen")+
+  scale_x_continuous(breaks = c(2013:2018))+
+  coord_cartesian(expand = F)+
+  themeEbird()+
+  theme(legend.position = c(0.8, 0.1),
+        legend.direction = "horizontal")+
+  labs(y = "observer score")
 
-# read in data and subset
-ebdNlSum <- ebd[between(longitude, box["xmin"], box["xmax"]) & between(latitude, box["ymin"], box["ymax"]),]
-
-# count obs now and keep obs with 10 or more
-ebdNlSum <- ebdNlSum[,.(nchk = length(unique(sampling_event_identifier))), 
-                     by = observer_id][nchk >= 10,]
-# change colnames
-setnames(ebdNlSum, c("observer", "nchk"))
-
-# add species seen and expertise score
-obsRptrScore <- fread("data/dataObsRptrScore.csv")
-dataSoi <- dataSoi[ebdNlSum, on = .(observer =observer)
-                   ][obsRptrScore, on=.(observer = observer)]
-dataSoi <- na.omit(dataSoi)
-
-# make plot data
-pltdata <- dataSoi[,.(meanSoi = mean(soiSeen),
-                      ciSoi = 1.96*sd(soiSeen)/sqrt(length(soiSeen))),
-                   by= .(plyr::round_any(ranefScore, 0.02))]
-
-setnames(pltdata, c("ranefScore","meanSoi","ciSoi"))
-
-# plot soiSeen ~ obsScore
-ggplot(pltdata)+
-  geom_pointrange(aes(ranefScore, meanSoi, ymin = meanSoi - ciSoi,
-                      ymax = meanSoi+ciSoi),
-                  col = "grey40")+
-  theme_classic()+
-  ylim(0, 10)+ xlim(0.2, 0.8)+
-  labs(x = 'ranef explorer score',
-       y = "# species of interest seen")
-
-ggsave("figs/figSoiSeenVsScore.png", width = 6, height = 6,
-       units = "in", dpi = 150)
-dev.off()
-
-# ends here
+# export
+ggsave(plotSoiScore, filename = "figs/figSoiScore.png", width = 8, height = 8,
+       device = png(), dpi = 300); dev.off()
