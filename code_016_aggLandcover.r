b291f67b2849a0dc142939e1e8befa6fddb35b7a
pratikunterwegs <pratikgupte16@gmail.com>
2019-09-26 20:55:57 +0200

    prepare 10m 1km comparison

--- code_016_aggLandcover.r
+++ code_016_aggLandcover.r
@@ -31,15 +31,38 @@ raster::writeRaster(rasterAgg1km, filename = "data/spatial/landcover1km.tif", fo
 #### compare rasters ####
 
 # load new rasters
-rasterAgg = raster("data/spatial/landcover100m.tif")
+library(dplyr)
+{rasterAgg = raster("data/spatial/landcover100m.tif")
 rasterAgg1km = raster("data/spatial/landcover1km.tif")
+}
+
+# make raster barplot data
+data1km = raster::getValues(rasterAgg1km); data1km = data1km[data1km > 0]; data1km = table(data1km); data1km = data1km/sum(data1km)
+
+{
+  data10m = raster::getValues(landcover); data10m = data10m[data10m > 0]
+  data10m = tibble(value = data10m)
+  data10m = dplyr::count(a, value) %>% dplyr::mutate(n=n/sum(n))
+  data10m = xtabs(n~value, data10m)
+}
 
 # map rasters
 {
-  x11()
+  png(filename = "figs/figLandcoverResample.png", width = 1200, height = 1200,
+      res = 150)
   par(mfrow=c(2,2))
-  plot(landcover, col = c("white", pals::kovesi.rainbow(7)))
-  plot(rasterAgg1km, col = c("white", pals::kovesi.rainbow(7)))
+  # rasterplots
+  raster::plot(landcover, col = c("white", pals::kovesi.rainbow(7)), 
+       main = "10m sentinel data", xlab = "longitude", y = "latitude")
+  raster::plot(rasterAgg1km, col = c("white", pals::kovesi.rainbow(7)),
+       main = "1km resampled data", xlab = "longitude", y = "latitude")
   
-  
+  # barplots
+  barplot(data10m, xlab = c("landcover class"), ylab = "prop.",
+          col = pals::kovesi.rainbow(7))
+  barplot(data1km, xlab = c("landcover class"), ylab = "prop.",
+          col = scales::alpha(pals::kovesi.rainbow(7), alpha =0.8), add = F)
+  barplot(data10m, xlab = c("landcover class"), ylab = "prop.",
+          col = "grey20", border = NA, density = 30, add = T)
+  dev.off()
 }
