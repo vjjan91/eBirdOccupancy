5fec0144a3e7264b18fd5164cf0989f469c2c4c9
Pratik Gupte <pratikgupte16@gmail.com>
2019-12-13 16:57:54 +0100

    distance to roads in python

--- code_21_dist2roads.py
+++ code_21_dist2roads.py
@@ -1,47 +1,56 @@
-from libpysal.weights.contiguity import Queen
-import libpysal
-from libpysal import examples
-import matplotlib.pyplot as plt
-
-import pandas_bokeh
-from splot.libpysal import plot_spatial_weights
-from bokeh.io import export_png
+# import classic python libs
+import itertools
+from operator import itemgetter
+import numpy as np
+import seaborn as sns
 
 # libs for dataframes
 import pandas as pd
 
 # import libs for geodata
-from shapely.geometry import Point, MultiPoint
 from shapely.ops import nearest_points
 import geopandas as gpd
+
+# import ckdtree
+from scipy.spatial import cKDTree
+from shapely.geometry import Point, MultiPoint, LineString
 
 # read in roads shapefile
 roads = gpd.read_file("data/spatial/roads_studysite_2019/roads_studysite_2019.shp")
 roads.head()
 
 # read in checklist covariates for conversion to gpd
-checklistCovars = pd.read_csv("data/eBirdChecklistVars.csv")
-gdf = gpd.GeoDataFrame(
-    checklistCovars,
-    geometry=gpd.points_from_xy(checklistCovars.longitude, checklistCovars.latitude))
+# get unique coordinates, assign them to the df
+# convert df to geo-df
+chkCovars = pd.read_csv("data/eBirdChecklistVars.csv")
+unique_locs = chkCovars.drop_duplicates(subset=['longitude','latitude'])[['longitude', 'latitude']]
+unique_locs['coordId'] = np.arange(1, unique_locs.shape[0]+1)
+chkCovars = chkCovars.merge(unique_locs, on=['longitude', 'latitude'])
+
+unique_locs = gpd.GeoDataFrame(
+   unique_locs,
+    geometry=gpd.points_from_xy(unique_locs.longitude, unique_locs.latitude))
+unique_locs.crs = {'init' :'epsg:4326'}
+
+# reproject spatials to 43n epsg 32642
+roads = roads.to_crs({'init': 'epsg:32642'})
+unique_locs = unique_locs.to_crs({'init': 'epsg:32642'})
 
 
-# define function for nearest neighbour
-def nearest(row, geom_union, df1, df2, geom1_col='geometry',
-            geom2_col='geometry', src_column=None):
-    """Find the nearest point and return the corresponding value from specified column."""
-    # Find the geometry that is closest
-    this_nearest = df2[geom2_col] == nearest_points(row[geom1_col], geom_union)[1]
-    # Get the corresponding value from df2 (matching is based on the geometry)
-    value = df2[this_nearest][src_column].get_values()[0]
-    return value
+# make a function based on geopandas distance
+def dist_to_road(point, roads):
+    return roads.distance(point).min()
 
 
-# make a unary union of the roads shapefile
-unary_union = roads.unary_union
+# get distance to nearest road
+unique_locs['min_dist_to_lines'] = unique_locs.geometry.apply(dist_to_road, args=(roads,))
+# plot histogram
+sns.distplot(a=unique_locs['min_dist_to_lines']/1e3)
 
-gdf['nearest_id'] = gdf.apply(nearest,
-                              geom_union=unary_union, df1=gdf, df2=roads,
-                              geom1_col='geometry', geom2_col="geometry",
-                              src_column = "osm_id",
-                              axis=1)
+# write to file
+unique_locs = pd.DataFrame(unique_locs.drop(columns='geometry'))
+unique_locs = unique_locs.drop(columns='min_dist_to_lines')
+unique_locs['dist_road'] = round(unique_locs['dist_road'], 2)
+unique_locs.to_csv(path_or_buf="data/locs_dist_to_road.csv", index=False)
+
+# ends here
