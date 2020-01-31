91ac216c17c444881098b9fb715381d2b7cbd7c5
pratikunterwegs <pratikgupte16@gmail.com>
2020-01-31 21:56:07 +0100

    update pycode network thinning

--- code/code_network_spatial_thinning.py
+++ code/code_network_spatial_thinning.py
@@ -19,62 +19,88 @@ from scipy.spatial import cKDTree
 # get unique coordinates, assign them to the df
 # convert df to geo-df
 chkCovars = pd.read_csv("data/eBirdChecklistVars.csv")
-
-ul = chkCovars.drop_duplicates(subset=['longitude','latitude'])[['longitude', 'latitude']]
+ul = chkCovars.drop_duplicates(subset=['longitude', 'latitude'])
+ul = ul.drop(columns='duration')
 ul['coordId'] = np.arange(0, ul.shape[0])
 
-# get effort proxy at each coord
-effort = chkCovars.groupby(['longitude', 'latitude'])['duration'].sum()
+# get effort at each coordinate
+effort = chkCovars.groupby(['longitude', 'latitude'])[
+    'duration'].sum()
 effort = effort.reset_index()
-effort['coordId'] = np.arange(0, effort.shape[0])
+
+# merge effort on ul
+ul = pd.merge(ul, effort, on=['longitude', 'latitude'])
 
 # make gpd and drop col from ul
 ulgpd = gpd.GeoDataFrame(ul,
               geometry=gpd.points_from_xy(ul.longitude, ul.latitude))
 ulgpd.crs = {'init' :'epsg:4326'}
-ul = pd.DataFrame(ul.drop(columns='geometry'))
-
 # reproject spatials to 43n epsg 32642
 ulgpd = ulgpd.to_crs({'init': 'epsg:32642'})
-
+ul = pd.DataFrame(ul.drop(columns="geometry"))
 
 # function to use ckdtrees for nearest point finding
 def ckd_pairs(gdfA, dist_indep):
-    nnbrs = len(gdfA.index)
     A = np.concatenate(
     [np.array(geom.coords) for geom in gdfA.geometry.to_list()])
     ckd_tree = cKDTree(A)
     dist = ckd_tree.query_pairs(r=dist_indep, output_type='ndarray')
     return dist
 
+# define scales in metres
+scales = [100, 500, 1000]
 
-site_pairs = ckd_pairs(ulgpd, 100)
-site_pairs = pd.DataFrame(data=site_pairs, columns=['p1', 'p2'])
+
+# function to process ckd_pairs
+def make_modules(scale):
+    site_pairs = ckd_pairs(gdfA=ulgpd, dist_indep=scale)
+    site_pairs = pd.DataFrame(data=site_pairs, columns=['p1', 'p2'])
+    site_pairs['scale'] = scale
+    # get site ids
+    site_id = np.concatenate((site_pairs.p1.unique(), site_pairs.p2.unique()))
+    site_id = np.unique(site_id)
+
+    # make network
+    network = nx.from_pandas_edgelist(site_pairs, 'p1', 'p2')
+    # get modules
+    modules = list(nx.algorithms.community.greedy_modularity_communities(network))
+    # get modules as df
+    m = []
+    for i in np.arange(len(modules)):
+        module_number = [i] * len(modules[i])
+        module_coords = list(modules[i])
+        m = m + list(zip(module_number, module_coords))
+
+    # add location and summed sampling duration
+    unique_locs = ul[ul.coordId.isin(site_id)][['longitude', 'latitude', 'coordId', 'duration']]
+    module_data = pd.DataFrame(m, columns=['module', 'coordId'])
+    module_data = pd.merge(module_data, unique_locs, on='coordId')
+    # add scale
+    module_data['scale'] = scale
+    return [site_pairs, module_data]
+
+
+# run make modules on ulgpd at scales
+data = list(map(make_modules, scales))
+
+# extract data for output
+tot_pair_data = []
+tot_module_data = []
+for i in np.arange(len(data)):
+    tot_pair_data.append(data[i][0])
+    tot_module_data.append(data[i][1])
+
+tot_pair_data = pd.concat(tot_pair_data, ignore_index=True)
+tot_module_data = pd.concat(tot_module_data, ignore_index=True)
 
 # make dict of positions and array of coordinates
-site_id = np.concatenate((site_pairs.p1.unique(), site_pairs.p2.unique()))
-site_id = np.unique(site_id)
-locations_df = ul[ul.coordId.isin(site_id)][['longitude', 'latitude']].to_numpy()
-pos_dict = dict(zip(site_id, locations_df))
-
-# make network and plot
-b = nx.from_pandas_edgelist(site_pairs, 'p1', 'p2')
-
-# find modules in the network and get effort to each
-modules = list(nx.algorithms.community.greedy_modularity_communities(b))
-
-m = []
-for i in np.arange(len(modules)):
-    module_number = [i]*len(modules[i])
-    module_coords = list(modules[i])
-    m = m + list(zip(module_number, module_coords))
-
-module_data = pd.DataFrame(m, columns=['module', 'coordId'])
-module_data = pd.merge(module_data, ul, on='coordId')
-module_data = pd.merge(module_data,
-                       effort[['duration', 'coordId']],
-                       on='coordId')
+# site_id = np.concatenate((site_pairs.p1.unique(), site_pairs.p2.unique()))
+# site_id = np.unique(site_id)
+# locations_df = ul[ul.coordId.isin(site_id)][['longitude', 'latitude']].to_numpy()
+# pos_dict = dict(zip(site_id, locations_df))
 
 # output data
-module_data.to_csv(path_or_buf="data/site_modules.csv", index=False)
-site_pairs.to_csv(path_or_buf="data/site_pairs.csv", index=False)
+tot_module_data.to_csv(path_or_buf="data/site_modules.csv", index=False)
+tot_pair_data.to_csv(path_or_buf="data/site_pairs.csv", index=False)
+
+# ends here
