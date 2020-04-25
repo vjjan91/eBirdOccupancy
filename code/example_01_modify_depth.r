c7af556cdba4881e3f8395619fff00bd2ffbe12a
pratikunterwegs <pratikgupte16@gmail.com>
2020-04-25 14:59:34 +0200

    add example of modify depth

--- code/example_01_modify_depth.r
+++ code/example_01_modify_depth.r
@@ -1,9 +1,21 @@
 library(tidyverse)
+# dataframe represents single species
 data = mtcars %>% 
-  split(mtcars$carb) %>% 
+  split(mtcars$carb) %>% # represents the hypothesis
   map(function(df){
-    split(df, df$gear)
+    split(df, df$gear) # represents the top models
   })
 
-# get the range of mpg at the second level
-modify_depth(data, .depth = 2, .f = function(df) range(df$mpg))
+# represents getting AIC
+data_range = modify_depth(data, .depth = 2, .f = function(tm) {
+  min(tm$mpg)
+})
+
+# represents getting AIC range
+data_range = modify_depth(data_range, .depth = 1, .f = function(tm) {
+  diff(range(unlist(tm)))
+})
+
+# now select the top level object with the min range
+# in this case, the first instance of 0 is selected
+data = data[which.min(unlist(data_range))]
